a7e65f2ee070a13e14c2df96b599985677935d3a
ilenisnerd <ilenisnerd@yandex.ru>
2025-02-28 19:25:53 +0300

    add v3 migration

--- migrations/V003__insert_data.sql
+++ migrations/V003__insert_data.sql
@@ -10,7 +10,7 @@ INSERT INTO product (id, name, picture_url, price) VALUES (6, 'Русская', 
 
 
 -- Заполнение таблицы orders данными
-INSERT INTO orders (id, status, date_created) SELECT i, (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)], DATE(NOW() - (random() * (NOW()+'90 days' - NOW()))) FROM generate_series(1, 100000) s(i);
+INSERT INTO orders (id, status, date_created) SELECT i, (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)], DATE(NOW() - (random() * (NOW()+'90 days' - NOW()))) FROM generate_series(1, 1000000) s(i);
 
 -- Заполнение таблицы order_product данными
-INSERT INTO order_product (quantity, order_id, product_id) SELECT floor(1+random()*50)::int, i, 1 + floor(random()*6)::int % 6 FROM generate_series(1, 100000) s(i);
+INSERT INTO order_product (quantity, order_id, product_id) SELECT floor(1+random()*50)::int, i, 1 + floor(random()*6)::int % 6 FROM generate_series(1, 1000000) s(i);
